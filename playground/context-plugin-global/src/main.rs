82e860859017b1c0c43f669323790898a8200478
Jason Asher <swo9@cdc.gov>
2025-06-06 16:44:41 -0400

    Update global lazy example

--- playground/context-plugin-global/src/main.rs
+++ playground/context-plugin-global/src/main.rs
@@ -1,152 +1,192 @@
-use ctor::ctor;
-use polonius_the_crab::prelude::*;
-use std::{
-    any::{Any, TypeId},
-    cell::{OnceCell, RefCell},
-    collections::{HashMap, HashSet},
-    sync::{LazyLock, Mutex},
-};
+pub mod context {
+    use polonius_the_crab::prelude::*;
+    use std::{
+        any::{Any, TypeId},
+        cell::{OnceCell, RefCell},
+        collections::{HashMap, HashSet},
+        sync::{LazyLock, Mutex},
+    };
 
-pub static DATA_PLUGINS: LazyLock<Mutex<RefCell<HashSet<TypeId>>>> =
-    LazyLock::new(|| Mutex::new(RefCell::new(HashSet::new())));
-pub struct Context {
-    data_plugins: HashMap<TypeId, OnceCell<Box<dyn Any>>>,
-}
+    pub static DATA_PLUGINS: LazyLock<Mutex<RefCell<HashSet<TypeId>>>> =
+        LazyLock::new(|| Mutex::new(RefCell::new(HashSet::new())));
 
-pub fn add_plugin_to_registry<T: DataPlugin>() {
-    DATA_PLUGINS
-        .lock()
-        .unwrap()
-        .borrow_mut()
-        .insert(TypeId::of::<T>());
-}
-
-impl Context {
-    pub fn new() -> Self {
-        let mut context = Context {
-            data_plugins: HashMap::new(),
-        };
-        // Register all data plugins from global list
-        for plugin_type_id in DATA_PLUGINS.lock().unwrap().borrow().iter() {
-            context
-                .data_plugins
-                .insert(*plugin_type_id, OnceCell::new());
-        }
-        context
-    }
-
-    pub fn get_data<T: DataPlugin>(&self, _plugin: T) -> &T::Value {
-        let type_id = TypeId::of::<T>();
-        self.data_plugins
-            .get(&type_id)
-            .expect("Data plugin not registered")
-            .get_or_init(|| Box::new(T::init(self)))
-            .downcast_ref::<T::Value>()
+    pub fn add_plugin_to_registry<T: DataPlugin>() {
+        DATA_PLUGINS
+            .lock()
             .unwrap()
+            .borrow_mut()
+            .insert(TypeId::of::<T>());
     }
 
-    pub fn get_data_mut<T: DataPlugin>(&mut self, _plugin: T) -> &mut T::Value {
-        let mut self_shadow = self;
-        let type_id = TypeId::of::<T>();
-        // If the data plugin is already initialized return mutable reference
-        // Use polonius to address borrow checker limitations
-        polonius!(|self_shadow| -> &'polonius mut T::Value {
-            let cell = self_shadow
-                .data_plugins
-                .get_mut(&type_id)
-                .expect("Data plugin not registered");
-            if let Some(any) = cell.get_mut() {
-                polonius_return!(any.downcast_mut::<T::Value>().unwrap());
+    pub struct Context {
+        data_plugins: HashMap<TypeId, OnceCell<Box<dyn Any>>>,
+    }
+
+    impl Context {
+        pub fn new() -> Self {
+            let mut context = Context {
+                data_plugins: HashMap::new(),
+            };
+            // Register all data plugins from global list
+            for plugin_type_id in DATA_PLUGINS.lock().unwrap().borrow().iter() {
+                context
+                    .data_plugins
+                    .insert(*plugin_type_id, OnceCell::new());
             }
-        });
-        // Initialize the data plugin
-        let data = T::init(self_shadow);
-        let cell = self_shadow.data_plugins.get_mut(&type_id).unwrap();
-        let _ = cell.set(Box::new(data));
-        cell.get_mut().unwrap().downcast_mut::<T::Value>().unwrap()
+            context
+        }
+
+        pub fn get_data<T: DataPlugin>(&self, _plugin: T) -> &T::DataContainer {
+            let type_id = TypeId::of::<T>();
+            self.data_plugins
+                .get(&type_id)
+                .expect("Data plugin not registered")
+                .get_or_init(|| Box::new(T::init(self)))
+                .downcast_ref::<T::DataContainer>()
+                .unwrap()
+        }
+
+        pub fn get_data_mut<T: DataPlugin>(&mut self, _plugin: T) -> &mut T::DataContainer {
+            let mut self_shadow = self;
+            let type_id = TypeId::of::<T>();
+            // If the data plugin is already initialized return mutable reference
+            // Use polonius to address borrow checker limitations
+            polonius!(|self_shadow| -> &'polonius mut T::DataContainer {
+                let cell = self_shadow
+                    .data_plugins
+                    .get_mut(&type_id)
+                    .expect("Data plugin not registered");
+                if let Some(any) = cell.get_mut() {
+                    polonius_return!(any.downcast_mut::<T::DataContainer>().unwrap());
+                }
+            });
+            // Initialize the data plugin
+            let data = T::init(self_shadow);
+            let cell = self_shadow.data_plugins.get_mut(&type_id).unwrap();
+            let _ = cell.set(Box::new(data));
+            cell.get_mut()
+                .unwrap()
+                .downcast_mut::<T::DataContainer>()
+                .unwrap()
+        }
+    }
+
+    pub trait DataPlugin: 'static {
+        type DataContainer;
+
+        fn init(context: &Context) -> Self::DataContainer;
+    }
+
+    #[macro_export]
+    macro_rules! define_data_plugin {
+        ($data_plugin:ident, $data_container:ty, $init:expr) => {
+            struct $data_plugin;
+
+            impl $crate::context::DataPlugin for $data_plugin {
+                type DataContainer = $data_container;
+
+                fn init(context: &$crate::context::Context) -> Self::DataContainer {
+                    $init(context)
+                }
+            }
+
+            paste::paste! {
+                #[ctor::ctor]
+                fn [<_register_plugin_$data_plugin:snake>]() {
+                    $crate::context::add_plugin_to_registry::<$data_plugin>()
+                }
+            }
+        };
     }
 }
 
-pub trait DataPlugin: 'static {
-    type Value;
+pub mod bool_plugin {
+    use super::context::*;
+    use super::define_data_plugin;
 
-    fn init(context: &Context) -> Self::Value;
-}
+    define_data_plugin!(BoolPlugin, bool, |_context| false);
 
-struct MyPlugin;
-impl DataPlugin for MyPlugin {
-    type Value = u32;
+    pub trait BoolExt {
+        fn set_bool(&mut self, value: bool);
+        fn get_bool(&self) -> bool;
+    }
 
-    fn init(_context: &Context) -> Self::Value {
-        println!("Initializing MyPlugin");
-        42
+    impl BoolExt for Context {
+        fn set_bool(&mut self, value: bool) {
+            *self.get_data_mut(BoolPlugin) = value;
+        }
+        fn get_bool(&self) -> bool {
+            *self.get_data(BoolPlugin)
+        }
     }
 }
-#[ctor]
-fn register_my_plugin() {
-    add_plugin_to_registry::<MyPlugin>()
+
+mod number_plugin {
+    use crate::bool_plugin::BoolExt;
+    use crate::{context::Context, define_data_plugin};
+
+    define_data_plugin!(NumberPlugin, u32, |_context| 0);
+
+    pub trait NumberExt {
+        fn set_number(&mut self, value: u32);
+        fn get_number(&self) -> u32;
+        fn get_bool_as_number(&self) -> u32;
+    }
+
+    impl NumberExt for Context {
+        fn set_number(&mut self, value: u32) {
+            *self.get_data_mut(NumberPlugin) = value;
+        }
+        fn get_number(&self) -> u32 {
+            *self.get_data(NumberPlugin)
+        }
+        fn get_bool_as_number(&self) -> u32 {
+            if self.get_bool() { 1 } else { 0 }
+        }
+    }
 }
+
+use bool_plugin::BoolExt;
+use context::*;
+
+use crate::number_plugin::NumberExt;
 
 pub fn main() {
     // Example usage
-    let context = Context::new();
-    assert_eq!(*context.get_data(MyPlugin), 42);
-    println!("{}", context.data_plugins.len());
+    let mut context = Context::new();
+    assert!(!context.get_bool());
+    assert_eq!(context.get_bool_as_number(), 0);
+    assert_eq!(context.get_number(), 0);
+    context.set_bool(true);
+    assert!(context.get_bool());
+    assert_eq!(context.get_bool_as_number(), 1);
+    context.set_number(2);
+    assert_eq!(context.get_number(), 2);
 }
 
 #[cfg(test)]
 mod test {
-    use ctor::ctor;
+    use super::context::Context;
+    use super::define_data_plugin;
 
-    use super::{Context, DataPlugin, add_plugin_to_registry};
+    define_data_plugin!(A, usize, |context: &Context| {
+        println!("Initializing A");
+        *context.get_data(B) + 1
+    });
 
-    struct A;
-    #[ctor]
-    fn register_a() {
-        add_plugin_to_registry::<A>()
-    }
+    define_data_plugin!(B, usize, |context: &Context| {
+        println!("Initializing B");
+        *context.get_data(C) + 1
+    });
 
-    impl DataPlugin for A {
-        type Value = usize;
-
-        fn init(context: &super::Context) -> Self::Value {
-            println!("Initializing A");
-            *context.get_data(B) + 1
-        }
-    }
-
-    struct B;
-    #[ctor]
-    fn register_b() {
-        add_plugin_to_registry::<B>()
-    }
-    impl DataPlugin for B {
-        type Value = usize;
-
-        fn init(context: &super::Context) -> Self::Value {
-            println!("Initializing B");
-            *context.get_data(C) + 1
-        }
-    }
-
-    struct C;
-    #[ctor]
-    fn register_c() {
-        add_plugin_to_registry::<C>()
-    }
-    impl DataPlugin for C {
-        type Value = usize;
-
-        fn init(_context: &super::Context) -> Self::Value {
-            println!("Initializing C");
-            0
-        }
-    }
+    define_data_plugin!(C, usize, |_context| {
+        println!("Initializing C");
+        0
+    });
 
     #[test]
     fn test() {
         let context = Context::new();
-        println!("{}", context.data_plugins.len());
 
         assert_eq!(*context.get_data(C), 0);
         assert_eq!(*context.get_data(A), 2);
@@ -156,7 +196,6 @@ mod test {
     #[test]
     fn test_mut() {
         let mut context = Context::new();
-        println!("{}", context.data_plugins.len());
 
         *context.get_data_mut(C) = 1;
         assert_eq!(*context.get_data(A), 3);
